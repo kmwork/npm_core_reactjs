b39ad9e3ccb8f172e695e1f2622dc4b55f01da7c
Konstantin Kakunin (Datana) <kakunin@lanit.ru>
2021-02-20 19:16:47 +0300

    Updates

---
+++ precache-manifest.a6f77058067a4ce6f805db05356e2ae9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe081fce1777060092ea158ae4208d7c",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "c15a9871148ebb30e296",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "b8fae29f368e64121baa",
+    "url": "/calculator/static/css/main.86f680a8.chunk.css"
+  },
+  {
+    "revision": "c15a9871148ebb30e296",
+    "url": "/calculator/static/js/2.846305b2.chunk.js"
+  },
+  {
+    "revision": "b8fae29f368e64121baa",
+    "url": "/calculator/static/js/main.41b2b517.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
